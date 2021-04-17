1818a5239540fcdc179a7698f6ab7695cc0a69eb
Prav Mukhi <praveshmukhi@gmail.com>
2021-04-17 17:49:28 -0400

    Completed Generate Password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Array of special characters to be included in password
+// array of special characters to be included in password
 var specialCharacters = [
   '@',
   '%',
@@ -25,10 +25,10 @@ var specialCharacters = [
   '.'
 ];
 
-// Array of numeric characters to be included in password
+// array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Array of lowercase characters to be included in password
+// array of lowercase characters to be included in password
 var lowerCasedCharacters = [
   'a',
   'b',
@@ -58,7 +58,7 @@ var lowerCasedCharacters = [
   'z'
 ];
 
-// Array of uppercase characters to be included in password
+// array of uppercase characters to be included in password
 var upperCasedCharacters = [
   'A',
   'B',
@@ -109,25 +109,30 @@ function getUserOptions() {
   lengthChosen = prompt("How many characters would you like your password to be (min. 8, max. 28)");
   lengthChosen = parseInt(lengthChosen)
 
-  //log type of length chosen - should be integer
+  //log type of length chosen - should be number
   console.log(typeof lengthChosen)
 
-// logging all variables to verify that correct value is stored in variable
-  console.log(lowerCaseChosen);
-  console.log(upperCaseChosen);
-  console.log(numericChosen);
-  console.log(specialChosen);
-  console.log(lengthChosen);
+// // logging all variables to verify that correct value is stored in variable
+  console.log("lower: ",lowerCaseChosen);
+  console.log("upper", upperCaseChosen);
+  console.log("numeric", numericChosen);
+  console.log("special", specialChosen);
+  console.log("length", lengthChosen);
 
-  // Validate the length of the password length input 
+  // validate the length of the password length input 
   if (lengthChosen < 8 || lengthChosen > 128) {
     alert ("Please select a value between 8-128");
+    return;
+  
   }
+
+  // Validate at least one group chosen
   if (!lowerCaseChosen && !upperCaseChosen && !numericChosen && !specialChosen) {
-    alert ("Please select at least one character group")
+    alert ("Please select at least one character group");
+    return;
   }
 
-
+// set variables for userOptions 
   var userOptions = {
     lowerCaseChosen: lowerCaseChosen,
     upperCaseChosen: upperCaseChosen,
@@ -136,8 +141,16 @@ function getUserOptions() {
     lengthChosen: lengthChosen,
   }
 
-  console.log(userOptions)
+// returns the userOptions 
+  return userOptions
 
+}
+
+// function for getting a random element from an array
+function getRandomFromLength(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+  return randElement;
 }
 
 // function to generate password
@@ -146,40 +159,61 @@ function generatePassword (){
   var userOptions = getUserOptions()
   var mustHaves = []
   var canHaves = []
-
     // if upper 
     if (userOptions.upperCaseChosen) {
-      // add upper char list to can haves
-      canHaves.concat(upperCaseChosen)
-      //add 1 upper char to must haves
-      mustHaves.push(upperCaseChosen[getRandomFromLength(upperCasedCharacters)])
-
-
+      console.log("canHaves:",canHaves);
+      // add char list to can haves
+      canHaves = canHaves.concat(upperCasedCharacters);
+      console.log("canHaves upperCaseCharacters:",canHaves);
+      //add 1 char to must haves
+      console.log("must-haves upperCase push:",upperCasedCharacters[getRandomFromLength(upperCasedCharacters)])
+      console.log("must-haves upperCase random:", getRandomFromLength(upperCasedCharacters))
+      mustHaves.push(getRandomFromLength(upperCasedCharacters));
+      console.log("mustHaves: ",mustHaves);
     }
 
       // add to possible charechters
    // if lower 
-      // add to mustHaves 1 upper char
-      // add to possible charechters
+   if (userOptions.lowerCaseChosen) {
+    // add char list to can haves
+    canHaves = canHaves.concat(lowerCasedCharacters);
+    //add 1 char to must haves
+    mustHaves.push(getRandomFromLength(lowerCasedCharacters));
+   }
+
    // if special 
-      // add to mustHaves 1 upper char
-      // add to possible charechters
-   // .
-   // .
-   // .
-   // loop through the lengthChosen
-      // add a random char to the passwordArray from the possible chars
-   // loop through must haves
-      // replace a chare in the password array
-   // passwordArray to string (join mathod)
+   if (userOptions.specialChosen) {
+    // add special char list to can haves
+    canHaves = canHaves.concat(specialCharacters);
+    //add 1 special char to must haves
+    mustHaves.push(getRandomFromLength(specialCharacters));
+   }
 
-  return password
+
+
+
+// loop through the lengthChosen  
+console.log("Must Haves: ", mustHaves)
+console.log("Length chosen", userOptions.lengthChosen)
+for (let currIndx = 0; currIndx < userOptions.lengthChosen; currIndx++) {
+
+     // add a random char to the passwordArray from the possible chars
+     password.push(getRandomFromLength(canHaves));
+     console.log("password:",password);
+
 }
+// loop through must haves
+for (let i = 0; i < mustHaves.length; i++) {
+       password[i] = mustHaves[i];
+     }
 
 
+// passwordArray to string (join mathod)
+  return password.join('')
+} 
 
 
-// Write password to the #password input
+// write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -187,5 +221,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
